a3c310814fa6ff79c8084ada2d0f4a4a73cef019
Enes Koyuncu <92369434+1910Enes@users.noreply.github.com>
2023-11-09 17:06:46 +0300

    Add files via upload
    ödev3_generateRandom

---
+++ 21060640_.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+int generateRandom() {
+  Random random = Random();
+  int randomNumber = random.nextInt(2);
+
+  if (randomNumber == 0) {
+    return 100;
+  } else {
+    return 0; 
+  }
+}
+
+void main() {
+  int status = generateRandom();
+  print('Durum: $status');
+}
